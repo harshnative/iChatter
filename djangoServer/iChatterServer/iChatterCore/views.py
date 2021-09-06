562ef994da69a79169bb1192376077c8a0b4c757
harshnative <harshnative@gmail.com>
2021-09-06 11:59:37 +0530

    added ssl server

--- djangoServer/iChatterServer/iChatterCore/views.py
+++ djangoServer/iChatterServer/iChatterCore/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse,Http404,HttpResponseBadRequest
+
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status
+
+
+import json
+
+
+@csrf_exempt
+def jarvis_version(request):
+
+    if(request.method == 'POST'):
+
+        key = str(request.POST["key"])
+
+        if(key == "2D5fCt9wW1UmwjBf"):
+            response = json.dumps([{'version':0.1}])
+            return HttpResponse(response , content_type='text/json')
+        else:
+            return HttpResponseBadRequest("invalid key")
+            
+    
+    else:
+        return HttpResponseBadRequest("API only available via POST method")
+
